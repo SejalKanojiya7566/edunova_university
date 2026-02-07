08cf8068996dfbf5396d17774434a13a9e555444
Sejal Kanojiya <sejalkanojiya890@gmail.com>
2026-02-07 08:58:24 -0500

    admin_sidebar.php

---
+++ admin/includes/admin_sidebar.php
@@ -0,0 +1,17 @@
+<div class="sidebar">
+    <div class="sidebar-header text-center">
+        <h5>EduNova University</h5>
+        <small>Admin Panel</small>
+    </div>
+
+    <a href="../dashboard/dashboard.php" class="nav-link">Dashboard</a>
+    <a href="../enquiries/view.php" class="nav-link">Enquiries</a>
+    <a href="../admissions/view.php" class="nav-link">Admissions</a>
+    <a href="../scholarships/view.php" class="nav-link">Scholarships</a>
+    <a href="../programs/view.php" class="nav-link">Programs</a>
+    <a href="../notifications/view.php" class="nav-link">Notifications</a>
+
+    <a href="../logout.php" class="nav-link text-danger mt-3">
+        <i class="fas fa-sign-out-alt"></i> Logout
+    </a>
+</div>
