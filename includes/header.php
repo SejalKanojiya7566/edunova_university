863593ec62f4409fffde66d868489184c2bb18d8
Sejal Kanojiya <sejalkanojiya890@gmail.com>
2026-02-07 08:41:38 -0500

    header.php

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>EduNova University</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- BOOTSTRAP CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- BOOTSTRAP ICONS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+
+<!-- GLOBAL CSS -->
+<link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+<body>
+
+<?php include("navbar.php"); ?>
