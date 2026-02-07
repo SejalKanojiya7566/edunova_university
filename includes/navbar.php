f266ba97c4a95f76d94cc6900fbf374adb050243
Sejal Kanojiya <sejalkanojiya890@gmail.com>
2026-02-07 08:42:14 -0500

    navbar.php

---
+++ includes/navbar.php
@@ -0,0 +1,64 @@
+<!-- PUBLIC NAVBAR -->
+<nav class="navbar navbar-expand-lg edu-navbar fixed-top shadow-sm">
+  <div class="container-fluid px-5">
+
+    <!-- LOGO -->
+    <a class="navbar-brand" href="index.php">
+      <img src="assets/images/logo.png" alt="EduNova Logo" height="45">
+    </a>
+
+    <!-- TOGGLER -->
+    <button class="navbar-toggler bg-light" type="button"
+            data-bs-toggle="collapse" data-bs-target="#eduNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- MENU -->
+    <div class="collapse navbar-collapse" id="eduNavbar">
+      <ul class="navbar-nav ms-auto align-items-center">
+
+        <!-- ABOUT -->
+        <li class="nav-item dropdown">
+          <a class="nav-link" href="#">About</a>
+          <div class="mega-menu">
+            <a href="about.php">About EduNova</a>
+            <a href="leadership.php">Leadership</a>
+            <a href="vision-mission.php">Vision & Mission</a>
+          </div>
+        </li>
+
+        <!-- PROGRAMS -->
+        <li class="nav-item dropdown">
+          <a class="nav-link" href="#">Programs</a>
+          <div class="mega-menu">
+            <a href="undergraduate.php">Undergraduate</a>
+            <a href="postgraduate.php">Postgraduate</a>
+            <a href="doctoral.php">Doctoral</a>
+          </div>
+        </li>
+
+        <!-- CAMPUSES -->
+        <li class="nav-item">
+          <a class="nav-link" href="campuses.php">Campuses</a>
+        </li>
+
+        <!-- PLACEMENTS -->
+        <li class="nav-item">
+          <a class="nav-link" href="placements.php">Placements</a>
+        </li>
+
+        <!-- RESEARCH -->
+        <li class="nav-item">
+          <a class="nav-link" href="research.php">Research</a>
+        </li>
+
+        <!-- APPLY -->
+        <li class="nav-item ms-3">
+          <a class="apply-btn" href="apply-now.php">APPLY NOW</a>
+        </li>
+
+      </ul>
+    </div>
+
+  </div>
+</nav>
