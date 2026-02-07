35c53fb6fdd3ee3963a196654140e55419b73d05
Sejal Kanojiya <sejalkanojiya890@gmail.com>
2026-02-07 08:33:26 -0500

    admin_header.php

---
+++ includes/admin_header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Admin Panel | EduNova University</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link href="../../assets/css/bootstrap.min.css" rel="stylesheet">
+<link href="../../assets/css/admin.css" rel="stylesheet">
+<link href="bootstrap-icons.css" rel="stylesheet">
+</head>
+<body>
